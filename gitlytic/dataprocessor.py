6248a8d26e31b923bf5bf1b7b83dfdb67017c0f0
Samuli Kupsala <samuli.kupsala@gmail.com>
2017-09-14 22:08:39 +0300

    Analyze and visualize project by repo
    Instead of analyzing single repo handle as a single project
    dataframe. Also plot by hour chart by repo to same chart.

--- gitlytic/dataprocessor.py
+++ gitlytic/dataprocessor.py
@@ -7,21 +7,19 @@ from project import get_project_output_dir
 
 
 def load_project_commits(project_path):
-    project_df = None
+    project_repo_df_list = []
     for f in os.listdir(get_project_output_dir(project_path)):
         if f.startswith('git_log_') and f.endswith('.tsv'):
             repo_name = f.replace('git_log_', '', 1).replace('.tsv', '', 1)
             repo_df = load_repo_commits(project_path, repo_name)
-            if project_df is None:
-                project_df = repo_df
-            else:
-                project_df = pd.concat([project_df, repo_df], verify_integrity=True)
-    return project_df
+            repo_df['repo_name'] = repo_name
+            project_repo_df_list.append(repo_df)
+    return pd.concat(project_repo_df_list, ignore_index=True, verify_integrity=True)
 
 
 def load_repo_commits(project_path, repo_name):
     logger.info('Loading commits dataframe for {}'.format(repo_name))
     project_output_dir = get_project_output_dir(project_path)
     repo_git_log_tsv_path = os.path.join(project_output_dir, 'git_log_{}.tsv'.format(repo_name))
-    return pd.DataFrame.from_csv(repo_git_log_tsv_path, sep=settings.FIELD_SEPARATOR,
+    return pd.DataFrame.from_csv(repo_git_log_tsv_path, index_col=None, sep=settings.FIELD_SEPARATOR,
                                  parse_dates=['author_date', 'committer_date'], infer_datetime_format=True)
